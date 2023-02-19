05cc99176080dc96de1b8fba12ba6f72268cda5a
Felix Yang <fzyang1227@gmail.com>
2023-02-19 17:18:02 -0500

    labs + initial set up

---
+++ public/tuiter/navigationSidebar/index.js
@@ -0,0 +1,20 @@
+
+const NavigationSidebar = () => {
+    $('#wd-explore').append(`
+      <div class="list-group">
+        <a class="list-group-item" href="/">
+          <i class="fab fa-twitter"></i></a>
+          <!-- continue rest of list, e.g.,
+               Home, Explore, Notifications,  Messages, etc. -->
+      </div>
+      <div class="d-grid mt-2">
+        <a href="tweet.html"
+           class="btn btn-primary btn-block rounded-pill">
+           Tweet</a>
+      </div>
+    `);
+   }
+
+$(NavigationSidebar)
+
+//export default NavigationSidebar;
